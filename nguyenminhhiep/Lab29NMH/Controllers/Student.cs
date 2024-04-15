82b39a7fcc0b019316804c295b80cda437a01af2
29022004zaasqs <166474458+29022004zaasqs@users.noreply.github.com>
2024-04-15 11:07:58 +0700

    123

---
+++ nguyenminhhiep/Lab29NMH/Controllers/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab29NMH.Controllers
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ClassName { get; set; }
+    }
+}
---
+++ nguyenminhhiep/Lab29NMH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab29NMH.MvcApplication" Language="C#" %>
